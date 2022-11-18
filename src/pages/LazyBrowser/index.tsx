d2d09fd295777202e8852de324d75e90d723d2ec
xiaoyu <xiaoyu@sulg.top>
2022-11-18 23:32:39 +0800

    完成图片懒加载

--- src/pages/LazyBrowser/index.tsx
+++ src/pages/LazyBrowser/index.tsx
@@ -6,7 +6,9 @@ export default function LazyBrowser() {
   return (
     <div className='lazy-browser'>
       {imageList.map((item) => (
-        <img className='image' loading='lazy' src={item} />
+        <div className='image' key={item}>
+          <img loading='lazy' src={item} />
+        </div>
       ))}
     </div>
   );
--- src/pages/LazyScroll/index.tsx
+++ src/pages/LazyScroll/index.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { imageList } from '@/utils/imageList';
+import useImageLazyScroll from './useImageLazyScroll';
+import './index.less';
 
 export default function LazyScroll() {
-  return <div>LazyScroll</div>;
+  const domRef = useRef([]);
+  const lazyScrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    getTop();
+    lazyScrollRef.current.addEventListener('scroll', getTop);
+    return () => lazyScrollRef.current.removeEventListener('scroll', getTop);
+  }, []);
+
+  const getTop = () => {
+    // 当前视窗的可视区域
+    let clientHeight = lazyScrollRef.current.clientHeight;
+    let len = domRef.current.length;
+    for (let i = 0; i < len; i++) {
+      // 元素距离页面顶部的距离
+      let { top } = domRef.current[i].getBoundingClientRect();
+      // 当图片减去可视区域高度小于等于0的时候，将data-src的值赋值给src
+      if (top - clientHeight <= 0) {
+        domRef.current[i].src = domRef.current[i].dataset.src;
+      }
+    }
+  };
+  return (
+    <div className='lazy-scroll' ref={lazyScrollRef}>
+      {imageList.map((item, index) => (
+        <img
+          className='image'
+          key={item}
+          ref={(e) => (domRef.current[index] = e)}
+          data-src={item}
+          src='/images/loading.gif'
+        />
+      ))}
+    </div>
+  );
 }
