c4f77eb3d1d007c0a31bb5a4ba057cb518148ef4
xiaoyu <xiaoyu@sulg.top>
2023-02-17 00:16:30 +0800

    完成lrudemo

--- src/pages/LRU/index.tsx
+++ src/pages/LRU/index.tsx
@@ -1,34 +1,62 @@
 import React, { useRef, useEffect, useState } from 'react';
 import LRUCache from '@/utils/lru';
-import { Button, Input } from 'antd';
+import { Button, Input, Tag } from 'antd';
 import './index.less';
 
 const DEFAULT_STORAGE_KEY = 'search';
 export default function LRU() {
   const [value, setValue] = useState('');
+  const [cacheList, setCacheList] = useState([]);
   const LRUCacheRef = useRef(null);
   useEffect(() => {
     LRUCacheRef.current = new LRUCache({
       size: 5,
       storageKey: DEFAULT_STORAGE_KEY,
     });
+    changeCacheList();
   }, []);
   const changeValue = (e) => {
     setValue(e.target.value);
   };
-  const putKey = () => {
-    LRUCacheRef.current.put(value, true);
+  const search = (data = null) => {
+    const search = data || value;
+    if (!search) {
+      return;
+    }
+    LRUCacheRef.current.put(search, true);
+    changeCacheList();
+  };
+
+  const tagSeach = (data) => {
+    setValue(data);
+    search(data);
+  };
+
+  const changeCacheList = () => {
+    const newList = [...LRUCacheRef.current.cache].map((item) => item[0]);
+    setCacheList(newList);
   };
   return (
     <div className='lru'>
-      <div>
+      <div className='search'>
         <Input
           placeholder='请输入搜索内容'
           value={value}
           onChange={changeValue}
         />
+        <Button onClick={() => search()}>search</Button>
       </div>
-      <Button onClick={putKey}>search</Button>
+
+      <div className='history'>
+        <div>搜索历史</div>
+        <div>
+          {cacheList.map((item) => (
+            <Tag key={item} onClick={() => tagSeach(item)}>
+              {item}
+            </Tag>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/utils/lru.ts
+++ src/utils/lru.ts
@@ -9,10 +9,8 @@ export default class LRUCache {
     if (storageKey) {
       this.storageKey = 'lru_storage_' + storageKey;
       const newList = JSON.parse(localStorage.getItem(this.storageKey));
-      console.log(newList);
       if (newList) {
         this.cache = new Map([...newList]);
-        console.log(this.cache);
         return;
       }
     }
@@ -45,10 +43,8 @@ export default class LRUCache {
   // 更新
   update(key, value) {
     this.cache.set(key, value);
-    console.log(this.cache);
     if (this.storageKey) {
       const newList = [...this.cache];
-      console.log(newList);
       localStorage.setItem(this.storageKey, JSON.stringify(newList));
     }
   }
